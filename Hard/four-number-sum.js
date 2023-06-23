98d1c7f672695cffc210cbb27a60ebef8d9541c3
Sonukr <sonuyadav9756@gmail.com>
2023-06-23 13:51:28 +0530

    fourNumberSum added more solutions

--- Hard/four-number-sum.js
+++ Hard/four-number-sum.js
@@ -55,7 +55,7 @@ Test Case 8
 }
 */
 
-function fourNumberSum(array, targetSum) {
+function fourNumberSumS1(array, targetSum) {
   // Write your code here.
   const quadruplets = []; // Initialize an empty array to store the quadruplets
   array.sort((a, b) => a - b); // Sort the array in ascending order
@@ -89,4 +89,74 @@ function fourNumberSum(array, targetSum) {
   return quadruplets; // Return the array of quadruplets
 }
 
+
+// Solution 2 using hash map
+function fourNumberSumS2(array, targetSum) {
+  const pairs = {}; // Create an empty hash map to store pairs of two numbers and their sums
+  const quadruplets = []; // Initialize an empty array to store the quadruplets
+
+  // Iterate through each pair of numbers and store their sums in the hash map
+  for (let i = 1; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const currentSum = array[i] + array[j];
+      const difference = targetSum - currentSum;
+
+      if (difference in pairs) {
+        // If the difference exists in the hash map, it means we have found a quadruplet
+        for (const pair of pairs[difference]) {
+          quadruplets.push([...pair, array[i], array[j]]);
+        }
+      }
+    }
+    // Store the current number and its sum with other numbers in the hash map
+    for (let k = 0; k < i; k++) {
+      const currentSum = array[i] + array[k];
+
+      if (currentSum in pairs) {
+        pairs[currentSum].push([array[k], array[i]]);
+      } else {
+        pairs[currentSum] = [[array[k], array[i]]];
+      }
+    }
+  }
+
+  return quadruplets; // Return the array of quadruplets
+
+}
+
+
+// Solution 3 using recursion
+function fourNumberSum(array, targetSum) {
+  // Recursive function to find all quadruplets
+  const fourNumberSumFunc = (array, targetSum, numCount = 0, currentSum = 0, currentQuadruplet = [], quadruplets = []) => {
+    // Base case: when numCount reaches 4
+    if (numCount === 4) {
+      // Check if the currentSum equals the targetSum
+      if (currentSum === targetSum) {
+        // Add a copy of the currentQuadruplet to the quadruplets array
+        quadruplets.push(currentQuadruplet.slice());
+      }
+      // Return the quadruplets array
+      return quadruplets;
+    }
+
+    // Iterate over the elements of the array
+    for (let i = 0; i < array.length; i++) {
+      const num = array[i];
+      // Add the current element to the currentQuadruplet
+      currentQuadruplet.push(num);
+      // Recursively call the function with the remaining elements,
+      // updated numCount, currentSum, currentQuadruplet, and quadruplets
+      fourNumberSumFunc(array.slice(i + 1), targetSum, numCount + 1, currentSum + num, currentQuadruplet, quadruplets);
+      // Remove the last element from the currentQuadruplet to backtrack
+      currentQuadruplet.pop();
+    }
+
+    // Return the quadruplets array
+    return quadruplets;
+  }
+
+  // Call the fourNumberSumFunc with the initial parameters
+  return fourNumberSumFunc(array, targetSum);
+}
 
